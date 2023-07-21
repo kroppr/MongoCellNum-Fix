be77b41d6eaf1d46fa6f0376c1721b32b06b40e3
kroppr <112423749+kroppr@users.noreply.github.com>
2023-07-21 10:12:35 -0500

    Add files via upload

--- cell#.py
+++ cell#.py
@@ -13,12 +13,22 @@ ourArray = csv_to_arr('./clients.AutomatedSMS_ReviewRequests_Copy.csv')
 for i in range(1, len(ourArray)):
     if not re.match("[0-9]{10}", ourArray[i][cellColumn]):
         this = ourArray[i][cellColumn]
-        this = this.replace("(", "")
-        this = this.replace(")", "")
-        this = this.replace("-", "")
-        this = this.replace(" ", "")
+        for c in this:
+            if not c.isdigit():
+                this = this.replace(c, "")
         ourArray[i][cellColumn] = this
+
 
 make_csv('./clients.AutomatedSMS_ReviewRequests.csv', ourArray)
 
 # Outputs array with gaps between every entry. Mongo ignores empty rows, so it works
+
+
+# Small Test
+
+'''
+for ele in ourArray:
+    for c in ourArray[i][cellColumn]:
+        if not c.isdigit():
+            print(ele[cellColumn])
+'''
